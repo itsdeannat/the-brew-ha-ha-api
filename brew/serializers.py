66e9af5a4c47ce338d1fbc8d307591b95b53a41a
Deanna Thompson <deanna.thompson45@outlook.com>
2024-08-29 19:20:56 -0400

    change: Modify field and add field validation

--- brew/serializers.py
+++ brew/serializers.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 import re
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(
+    username = serializers.CharField(
         required=True,
         error_messages={
-            "required": "An email is required.",
+            "required": "A username is required.",
         }
     )
     password = serializers.CharField(
@@ -18,20 +18,21 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'password')
+        fields = ('username', 'password')
+    
+    def validate_username(self, username):
+        if not re.match("^[a-zA-Z\\d]*$", username):
+            raise serializers.ValidationError({"message": "Username can only contain letters and numbers."})
+        return username
 
+    def validate_password(self, password):
+        if not re.match("^[a-zA-Z\\d]*$", password):
+            raise serializers.ValidationError({"message": "Password can only contain letters and numbers."})      
+        return password
+        
     def create(self, validated_data):
         user = User.objects.create_user(
-            email=validated_data['email'],
+            username=validated_data['username'],
             password=validated_data['password']
         )
         return user
-    
-    def validate_email(self, email):
-        # Check to make sure email format is valid
-        if not re.match("([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\\.[A-Z|a-z]{2,})+", email):
-            raise serializers.ValidationError("Email format is invalid.")
-
-    def validate_last_name(self, password):
-        if not re.match("^[a-zA-Z\\d]*$", password):
-            raise serializers.ValidationError("Password can only contain letters and numbers.")
