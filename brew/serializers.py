fdc12018d87e9c69400fbdbc424daad84976cbb8
Deanna Thompson <deanna.thompson45@outlook.com>
2024-08-29 17:06:21 -0400

    change: Modify UserRegistrationSerializer fields

--- brew/serializers.py
+++ brew/serializers.py
@@ -1,17 +1,37 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+import re
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(
+        required=True,
+        error_messages={
+            "required": "An email is required.",
+        }
+    )
+    password = serializers.CharField(
+        required=True,
+        error_messages={
+            "required": "A password is required.",
+        }
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'email')
+        fields = ('email', 'password')
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username=validated_data['username'],
-            password=validated_data['password'],
-            email=validated_data.get('email', '')
+            email=validated_data['email'],
+            password=validated_data['password']
         )
         return user
+    
+    def validate_email(self, email):
+        # Check to make sure email format is valid
+        if not re.match("([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\\.[A-Z|a-z]{2,})+", email):
+            raise serializers.ValidationError("Email format is invalid.")
+
+    def validate_last_name(self, password):
+        if not re.match("^[a-zA-Z\\d]*$", password):
+            raise serializers.ValidationError("Password can only contain letters and numbers.")
