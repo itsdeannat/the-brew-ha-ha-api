7b1ffa525e32c9fad6a32f7da02ef09c3fe8f61d
Deanna Thompson <deanna.thompson45@outlook.com>
2024-08-29 19:21:39 -0400

    feat: Add tests for registration view

--- brew/tests.py
+++ brew/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
 # Create your tests here.
+class RegistrationTestCalls(TestCase):
+        
+    def test_registration_view_valid(self):
+        response = self.client.post('/register/', data={
+            'username': 'johndoe',
+            'password': 'password123',
+        }, follow=True)
+        self.assertEqual(response.status_code, 201)
+        
+    def test_registration_view_missing_user_name(self):
+        response = self.client.post('/register/', data={
+            'username': '',
+            'password': 'pass$word123',
+        }, follow=True)
+        self.assertEqual(response.status_code, 400)
+        
+    def test_registration_view_bad_data(self):
+        response = self.client.post('/register/', data={
+            'username': '$asdk)##$',
+            'password': 'pass$worSDFd123',
+        }, follow=True)
+        self.assertEqual(response.status_code, 400)
