ce2b94a22428281ca0e8f61fc06479bbe451b1cd
Deanna Thompson <deanna.thompson45@outlook.com>
2024-08-29 19:21:53 -0400

    change: Rename register url

--- brew/urls.py
+++ brew/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import UserRegistrationView
 
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='user_registration')
+    path('register/', UserRegistrationView.as_view(), name='register')
 ]
