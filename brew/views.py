0be1d8c89b4a0863abac030e46fe2a972c48585d
Deanna Thompson <deanna.thompson45@outlook.com>
2024-08-30 19:23:26 -0400

    feat: Add a ping endpoint for users

--- brew/views.py
+++ brew/views.py
@@ -3,6 +3,9 @@ from rest_framework.views import APIView
 from drf_spectacular.utils import extend_schema
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 from .serializers import UserRegistrationSerializer
 
 # Create your views here.
@@ -16,3 +19,14 @@ class UserRegistrationView(APIView):
             serializer.save()
             return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PingView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    @extend_schema(exclude=True)
+    def get(self, request, format=None):
+        content = {
+            "message": "Test ping successful!"
+        }
+        return Response(content, status=status.HTTP_200_OK)
