fafbc9a41efd7e5192ae04a0c1d425251990ac99
Deanna Thompson <deanna.thompson45@outlook.com>
2024-08-30 14:33:12 -0400

    feat: Add tests for JWT authentication

--- brew/tests.py
+++ brew/tests.py
@@ -32,3 +32,30 @@ class RegistrationTestCalls(TestCase):
             'password': 'pass$worSDFd123',
         }, follow=True)
         self.assertEqual(response.status_code, 400)
+        
+class JWTAuthenticationCalls(TestCase):
+    
+    def test_registered_user(self):
+        '''
+        Tests that registered user can retrieve JWT
+        '''
+        user_data = {
+            'username': 'johndoe',
+            'password': 'password123'
+        }
+        response = self.client.post('/register/', user_data, format='json')
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.post('/api/token/', user_data, follow=True)
+        self.assertEqual(response.status_code, 200)
+        
+    def test_unregistered_user(self):
+        '''
+        Tests that unregistered user can't receive JWT
+        '''
+        user_data = {
+            'username': 'johndoe',
+            'password': 'password123'
+        }
+        response = self.client.post('/api/token/', user_data, follow=True)
+        self.assertEqual(response.status_code, 401)
