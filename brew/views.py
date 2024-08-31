026a8ca919b89a8dfcdae733694857a1c2106c12
Deanna Thompson <deanna.thompson45@outlook.com>
2024-08-31 11:50:31 -0400

    fix: Modify success message for signup

--- brew/views.py
+++ brew/views.py
@@ -17,7 +17,7 @@ class UserSignupView(APIView):
         serializer = UserSignupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+            return Response({'message': 'Signup successful!'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PingView(APIView):
