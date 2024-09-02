8d9d40e0b7557888f55a96f91d2f74f3fada94e0
Deanna Thompson <deanna.thompson45@outlook.com>
2024-09-01 22:25:08 -0400

    feat: Add the coffee and snack serializers

--- brew/serializers.py
+++ brew/serializers.py
@@ -1,8 +1,63 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 import re
+from .models import Coffee
+from .models import Snack
 
+class CoffeeSerializer(serializers.ModelSerializer):
+    """Serializer for the coffee model. 
+    
+    Converts Coffee model instances into JSON format.
+
+    Fields:
+        id (int): The unique identifier for the coffee.
+        coffee_type (str): The name or type of the coffee, e.g., 'Espresso', 'Latte'.
+        temperature (str): The serving temperature of the coffee, e.g., 'Hot', 'Iced'.
+        caffeine_amount (int): The amount of caffeine in milligrams.
+        price (float): The price of the coffee in USD.
+    """
+    class Meta:
+        model=Coffee
+        fields=['id', 'coffee_type', 'temperature', 'caffeine_amount', 'price']
+        
+class SnackSerializer(serializers.ModelSerializer):
+    """Serializer for the snack model. 
+    
+    Converts Snack model instances into JSON format.
+
+    Fields:
+        id (int): The unique identifier for the snack.
+        snack_name (str): The name of the snack, e.g. 'Muffin', 'Chips'
+        price (float): The price of the snack in USD.
+    """
+    
+    snack_name = serializers.CharField(help_text='The name of the snack')
+    price = serializers.FloatField(help_text='The price of the coffee in USD')
+    class Meta:
+        model=Snack
+        fields=['id', 'snack_name', 'price']
+    
 class UserSignupSerializer(serializers.ModelSerializer):
+    """Serializer for user signups. 
+    
+    Validates and creates a user account with a username and password.
+
+    Fields:
+        username (str): The user's username. Must be alphanumeric.
+        password (str): The user's password. Must be alphanumeric.
+
+    Raises:
+        serializers.ValidationError: If the username or password is not alphanumeric.
+        
+    Methods:
+        def_validate_username(username): Validates that username is alphanumeric. 
+        def_validate_password(password): Validates that password is alphanumeric. 
+        def_create: Creates a new user with validated data
+
+    Returns:
+        User: the new User instance
+    """
+    
     username = serializers.CharField(
         required=True,
         error_messages={
