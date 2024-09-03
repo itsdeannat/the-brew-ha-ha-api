38761a187f2e8f5ca219d57988f6fe3fc0abc1f0
Deanna Thompson <deanna.thompson45@outlook.com>
2024-09-03 16:53:29 -0400

    feat: Update urls to get resource by id

--- brew/urls.py
+++ brew/urls.py
@@ -3,11 +3,15 @@ from .views import UserSignupView
 from .views import PingView
 from .views import GetAllCoffeesView
 from .views import GetAllSnacksView
+from .views import GetCoffeeByIdView
+from .views import GetSnackByIdView
 
 
 urlpatterns = [
     path('api/signup/', UserSignupView.as_view(), name='signup'),
     path('api/ping/', PingView.as_view(), name='ping'),
-    path('api/coffees/', GetAllCoffeesView.as_view(), name='coffees'),
-    path('api/snacks/', GetAllSnacksView.as_view(), name='snacks')
+    path('api/coffees/', GetAllCoffeesView.as_view(), name='get_all_coffees'),
+    path('api/snacks/', GetAllSnacksView.as_view(), name='get_all_snacks'),
+    path('api/coffees/<int:pk>/', GetCoffeeByIdView.as_view(), name='get_coffee_by_id'),
+    path('api/snacks/<int:pk>', GetSnackByIdView.as_view(), name='get_snack_by_id')    
 ]
