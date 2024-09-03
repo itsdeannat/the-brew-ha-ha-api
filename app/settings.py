eee741263f5bc8962762f14af3048d76d13952db
Deanna Thompson <deanna.thompson45@outlook.com>
2024-09-02 21:40:58 -0400

    feat: Add tag descriptions

--- app/settings.py
+++ app/settings.py
@@ -147,4 +147,18 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'The Brew Ha Ha API allows you to programmatically access our products, securely manage orders, and streamline purchases.',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
+    'TAGS': [
+        {
+            'name': 'coffees',
+            'description': 'Represents a coffee drink in the Brew Ha Ha inventory.'
+        },
+        {
+            'name': 'snacks',
+            'description': 'Represents a snack in the Brew Ha Ha inventory.'
+        },
+        {
+            'name': 'token',
+            'description': 'Use these endpoints generate a JSON Web Token (JWT) to authenticate your credentials.'
+        }
+    ]
 }
