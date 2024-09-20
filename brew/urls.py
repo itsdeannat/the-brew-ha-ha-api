f039c8a8b5db915ac6c1f12128246b90260b8754
Deanna Thompson <deanna.thompson45@outlook.com>
2024-09-20 16:51:21 -0400

    feat: Add url for orders

--- brew/urls.py
+++ brew/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import UserSignupView
 from .views import PingView
 from .views import ProductViewSet
+from .views import CreateOrderView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
@@ -11,5 +12,6 @@ router.register(r'products', ProductViewSet, basename='product')
 urlpatterns = [
     path('api/signup/', UserSignupView.as_view(), name='signup'),
     path('api/ping/', PingView.as_view(), name='ping'),
+    path('api/orders/', CreateOrderView.as_view(), name='create_order'),
     path('api/', include(router.urls)) 
 ]
