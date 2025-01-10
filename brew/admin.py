897766a50b3bbdd910e57859db96bbc7de11927f
Deanna Thompson <deanna.thompson45@outlook.com>
2025-01-10 16:09:59 -0500

    change: Update admin file

--- brew/admin.py
+++ brew/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Product 
+from .models import Order
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'temperature', 'caffeine_amount', 'price', 'description', 'quantity')  
 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('payment_method', 'order_date', 'status')
