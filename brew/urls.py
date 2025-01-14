b27e7b3f86b5841ef55df4971f3603247c453707
Deanna Thompson <deanna.thompson45@outlook.com>
2025-01-14 10:09:56 -0500

    change: Modify urls.py to use new viewset

--- brew/urls.py
+++ brew/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import UserSignupView
 from .views import PingView
 from .views import ProductViewSet
-from .views import CreateOrderView
+from .views import OrderViewSet
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
@@ -12,6 +12,10 @@ router.register(r'products', ProductViewSet, basename='product')
 urlpatterns = [
     path('api/signup/', UserSignupView.as_view(), name='signup'),
     path('api/ping/', PingView.as_view(), name='ping'),
-    path('api/orders/', CreateOrderView.as_view(), name='create_order'),
+    path('api/orders/', OrderViewSet.as_view({
+        'get': 'retrieve',
+        'post': 'create'
+        }),
+        name='create_order'),
     path('api/', include(router.urls)) 
 ]
